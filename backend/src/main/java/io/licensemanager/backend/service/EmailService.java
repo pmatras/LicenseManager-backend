45aac69021c4a54ec442e6047295002938144c8c
Piotr Matras <piotrmatras21@gmail.com>
2020-10-24 19:38:59 +0200

    implemented e-mail service for sending html and plain text e-mails

---
+++ backend/src/main/java/io/licensemanager/backend/service/EmailService.java
@@ -0,0 +1,54 @@
+package io.licensemanager.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class EmailService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserAccountService.class);
+
+    @Value("${mail.no-reply.address:no-reply@license-manager.io}")
+    private String NO_REPLY_EMAIL_ADDRESS;
+    private final JavaMailSender mailSender;
+
+    public void sendEmailMessage(String subject, String recipient, String content) {
+        SimpleMailMessage emailMessage = new SimpleMailMessage();
+        emailMessage.setFrom(NO_REPLY_EMAIL_ADDRESS);
+        emailMessage.setTo(recipient);
+        emailMessage.setSubject(subject);
+        emailMessage.setText(content);
+
+        mailSender.send(emailMessage);
+    }
+
+    public boolean sendHtmlEmailMessage(String subject, String recipient, String content) {
+        MimeMessage emailMessage = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(emailMessage, true);
+            helper.setFrom(NO_REPLY_EMAIL_ADDRESS);
+            helper.setTo(recipient);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+
+            mailSender.send(emailMessage);
+        } catch (MessagingException e) {
+            logger.error("Failed to send e-mail, reason: {}", e.getMessage());
+
+            return false;
+        }
+
+        return true;
+    }
+}
