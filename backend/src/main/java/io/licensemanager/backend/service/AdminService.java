089e34589518d8dac0a0472eb4d070c5a6cf41ba
Piotr Matras <piotrmatras21@gmail.com>
2020-10-29 19:26:44 +0100

    implemented user account activation, enabling, disabling by admin

---
+++ backend/src/main/java/io/licensemanager/backend/service/AdminService.java
@@ -0,0 +1,61 @@
+package io.licensemanager.backend.service;
+
+import io.licensemanager.backend.entity.User;
+import io.licensemanager.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class AdminService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AdminService.class);
+
+    private final UserRepository userRepository;
+
+    public Optional<User> activateUserByAdmin(final Long userId) {
+        logger.info("Activating user account by admin");
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent() && !user.get().getIsAccountActivatedByAdmin()) {
+            User userToActivate = user.get();
+            userToActivate.setIsAccountActivatedByAdmin(true);
+            return Optional.of(userRepository.save(userToActivate));
+        }
+        logger.warn("Failed to activate user by admin - user doesn't exist or is already activated");
+
+        return Optional.empty();
+    }
+
+    public Optional<User> enableUserAccount(final Long userId) {
+        logger.info("Enabling user account by admin");
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent() && !user.get().getIsActive()) {
+            User userToEnable = user.get();
+            userToEnable.setIsActive(true);
+
+            return Optional.of(userRepository.save(userToEnable));
+        }
+        logger.warn("Failed to enable user by admin - user doesn't exist or is already enabled");
+
+        return Optional.empty();
+    }
+
+    public Optional<User> disableUserAccount(final Long userId) {
+        logger.info("Disabling user account by admin");
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent() && user.get().getIsActive()) {
+            User userToDisable = user.get();
+            userToDisable.setIsActive(false);
+
+            return Optional.of(userRepository.save(userToDisable));
+        }
+        logger.warn("Failed to disable user by admin - user doesn't exist or is already disabled");
+
+        return Optional.empty();
+    }
+}
