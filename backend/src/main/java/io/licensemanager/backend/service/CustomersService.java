522ccba20e9569e3bb533e4f7a45191a50f49de3
Piotr Matras <piotrmatras21@gmail.com>
2020-11-20 22:33:46 +0100

    added filtering groups to which customer was assigned to only owned by user which sends request in case of sharing multiple customers across users
    loggers added

--- backend/src/main/java/io/licensemanager/backend/service/CustomersService.java
+++ backend/src/main/java/io/licensemanager/backend/service/CustomersService.java
@@ -17,6 +17,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -28,20 +29,36 @@ public class CustomersService {
     private final CustomerGroupRepository customerGroupRepository;
     private final UserRepository userRepository;
 
+    private List<Customer> filterCustomersGroups(List<Customer> customers, final User creator) {
+        return customers.stream()
+                .map(customer -> {
+                    Set<CustomerGroup> groupsOwnedByUser = customer.getGroups().stream()
+                            .filter(group -> group.getCreator().equals(creator))
+                            .collect(Collectors.toSet());
+                    customer.setGroups(groupsOwnedByUser);
+                    return customer;
+                })
+                .collect(Collectors.toList());
+    }
+
     public List<Customer> getCustomersList(final String username, final Set<ROLES_PERMISSIONS> userPermissions) {
+        logger.debug("Getting customers list");
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isEmpty()) {
+            logger.error("Cannot find user which requested customers list");
+            return Collections.emptyList();
+        }
+        User creator = user.get();
         if (userPermissions.contains(ROLES_PERMISSIONS.VIEW_ALL_CUSTOMERS)
                 || userPermissions.contains(ROLES_PERMISSIONS.ALL)) {
-            return customerRepository.findAll();
-        }
-        Optional<User> user = userRepository.findByUsername(username);
-        if (user.isPresent()) {
-            return customerRepository.findAllByCreatorIs(user.get());
+            return filterCustomersGroups(customerRepository.findAll(), creator);
         }
 
-        return Collections.emptyList();
+        return filterCustomersGroups(customerRepository.findAllByCreatorIs(creator), creator);
     }
 
     public List<CustomerGroup> getCustomersGroupsList(final String username) {
+        logger.debug("Getting customer groups list");
         Optional<User> user = userRepository.findByUsername(username);
         if (user.isPresent()) {
             return customerGroupRepository.findAllByCreatorIs(user.get());
