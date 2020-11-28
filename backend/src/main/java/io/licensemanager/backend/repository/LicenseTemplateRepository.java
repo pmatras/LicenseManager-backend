ed698768fedb925b98022eefcbf7cae3ecbc548d
Piotr Matras <piotrmatras21@gmail.com>
2020-11-28 22:18:47 +0100

    added license template service

--- backend/src/main/java/io/licensemanager/backend/repository/LicenseTemplateRepository.java
+++ backend/src/main/java/io/licensemanager/backend/repository/LicenseTemplateRepository.java
@@ -3,5 +3,8 @@ package io.licensemanager.backend.repository;
 import io.licensemanager.backend.entity.LicenseTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LicenseTemplateRepository extends JpaRepository<LicenseTemplate, Long> {
+    Optional<LicenseTemplate> findByName(final String templateName);
 }
---
+++ backend/src/main/java/io/licensemanager/backend/service/LicenseTemplateService.java
@@ -0,0 +1,83 @@
+package io.licensemanager.backend.service;
+
+import io.licensemanager.backend.entity.LicenseTemplate;
+import io.licensemanager.backend.entity.User;
+import io.licensemanager.backend.repository.LicenseTemplateRepository;
+import io.licensemanager.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class LicenseTemplateService {
+
+    private static final Logger logger = LoggerFactory.getLogger(LicenseTemplateService.class);
+
+    private final LicenseTemplateRepository licenseTemplateRepository;
+    private final UserRepository userRepository;
+
+    public List<String> getSupportedFieldTypes() {
+        return Stream.of(SUPPORTED_FIELDS_TYPES.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Optional<LicenseTemplate> createLicenseTemplate(final String name, final Map<String, String> fields,
+                                                           final String creatorsUsername) {
+        logger.debug("Creating new license template with name {}", name);
+        Optional<LicenseTemplate> existingTemplate = licenseTemplateRepository.findByName(name);
+        if (existingTemplate.isEmpty()) {
+            Optional<User> creator = userRepository.findByUsername(creatorsUsername);
+            if (creator.isEmpty()) {
+                logger.error("Cannot create requested license template - user with passed username doesn't exist");
+                return Optional.empty();
+            }
+            LicenseTemplate template = new LicenseTemplate();
+            template.setName(name);
+            template.setFields(parseFieldTypes(fields));
+            template.setCreationTime(LocalDateTime.now());
+            template.setCreator(creator.get());
+
+            return Optional.of(licenseTemplateRepository.save(template));
+        }
+        logger.error("License with requested name already exists, creation skipped");
+
+        return Optional.empty();
+    }
+
+    private Map<String, Class> parseFieldTypes(final Map<String, String> fields) {
+        Map<String, Class> convertedFields = new HashMap<>();
+        fields.forEach((key, value) -> {
+            try {
+                convertedFields.put(key, Class.forName(String.format("java.lang.%s", value)));
+            } catch (ClassNotFoundException e) {
+                logger.error("{} isn't correct type name, this template field will be omitted", value);
+            }
+        });
+
+        return convertedFields;
+    }
+}
+
+enum SUPPORTED_FIELDS_TYPES {
+    String,
+    Integer,
+    Long,
+    Float,
+    Double,
+    Boolean,
+    Character
+}
