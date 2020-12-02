e2a1721b9a79a9d5259f017480f1e4a070ef4c44
Piotr Matras <piotrmatras21@gmail.com>
2020-12-02 13:44:01 +0100

    added license entity with its repository

---
+++ backend/src/main/java/io/licensemanager/backend/entity/License.java
@@ -0,0 +1,40 @@
+package io.licensemanager.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "licenses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class License {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "license_id")
+    private Long id;
+
+    private String name;
+
+    private String licenseKey;
+
+    private LocalDateTime generationDate;
+
+    private LocalDateTime expirationDate;
+
+    @Lob
+    private byte[] licenseFile;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "template_id")
+    private LicenseTemplate usedTemplate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+}
---
+++ backend/src/main/java/io/licensemanager/backend/repository/LicenseRepository.java
@@ -0,0 +1,7 @@
+package io.licensemanager.backend.repository;
+
+import io.licensemanager.backend.entity.License;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LicenseRepository extends JpaRepository<License, Long> {
+}
