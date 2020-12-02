853a1e4c38a132e507408a053b163aeb884962a4
Piotr Matras <piotrmatras21@gmail.com>
2020-12-02 18:36:41 +0100

    changed content's encryption from RSA to AES with key generated from public RSA key
    RSA has limitations on bytes length of content to encrypt

--- backend/src/main/java/io/licensemanager/backend/util/CryptoUtils.java
+++ backend/src/main/java/io/licensemanager/backend/util/CryptoUtils.java
@@ -7,6 +7,7 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.util.Base64;
@@ -17,15 +18,18 @@ public class CryptoUtils {
     private static final Logger logger = LoggerFactory.getLogger(CryptoUtils.class);
 
     private static final String ASYMMETRIC_ALGORITHM = "RSA";
-    private static final Integer KEY_LENGTH = 1024;
+    private static final int RSA_KEY_LENGTH = 1024;
+    private static final String SYMMETRIC_ALGORITHM = "AES";
+    private static final int AES_KEY_LENGTH = 128;
     private static final String SIGNING_ALGORITHM = "SHA256withRSA";
+    private static final short BITS_IN_BYTE = 8;
 
     public static Optional<KeyPair> generateKeyPair() {
         logger.info("Generating new key pair for asymmetric files signing");
         KeyPair keyPair = null;
         try {
             KeyPairGenerator generator = KeyPairGenerator.getInstance(ASYMMETRIC_ALGORITHM);
-            generator.initialize(KEY_LENGTH, new SecureRandom());
+            generator.initialize(RSA_KEY_LENGTH, new SecureRandom());
             keyPair = generator.generateKeyPair();
         } catch (NoSuchAlgorithmException e) {
             logger.error("Cannot generate key pair, reason - {}", e.getMessage());
@@ -34,12 +38,24 @@ public class CryptoUtils {
         return Optional.ofNullable(keyPair);
     }
 
-    public static byte[] encrypt(final String plainText, final PrivateKey privateKey) {
+    private static Key generateAESKeyFromRSAKey(final Key rsaKey) {
+        logger.info("generating AES key");
+        byte[] aesKeyBytes = new byte[AES_KEY_LENGTH / BITS_IN_BYTE];
+        byte[] rsaKeyBytes = rsaKey.getEncoded();
+        int aesOffset = rsaKeyBytes.length / aesKeyBytes.length;
+        for (int i = 0; i < aesKeyBytes.length; ++i) {
+            aesKeyBytes[i] = rsaKeyBytes[i * aesOffset];
+        }
+
+        return new SecretKeySpec(aesKeyBytes, SYMMETRIC_ALGORITHM);
+    }
+
+    public static byte[] encrypt(final String plainText, final PublicKey publicKey) {
         logger.info("Encrypting content with public key");
         byte[] encrypted = new byte[0];
         try {
-            Cipher encryptCipher = Cipher.getInstance(ASYMMETRIC_ALGORITHM);
-            encryptCipher.init(Cipher.ENCRYPT_MODE, privateKey);
+            Cipher encryptCipher = Cipher.getInstance(SYMMETRIC_ALGORITHM);
+            encryptCipher.init(Cipher.ENCRYPT_MODE, generateAESKeyFromRSAKey(publicKey));
             encrypted = encryptCipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
             logger.error("Failed to encrypt content, reason - {}", e.getMessage());
@@ -52,8 +68,8 @@ public class CryptoUtils {
         logger.info("Decrypting content with public key");
         String decrypted = "";
         try {
-            Cipher decryptCipher = Cipher.getInstance(ASYMMETRIC_ALGORITHM);
-            decryptCipher.init(Cipher.DECRYPT_MODE, publicKey);
+            Cipher decryptCipher = Cipher.getInstance(SYMMETRIC_ALGORITHM);
+            decryptCipher.init(Cipher.DECRYPT_MODE, generateAESKeyFromRSAKey(publicKey));
             decrypted = new String(decryptCipher.doFinal(encrypted), StandardCharsets.UTF_8);
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | BadPaddingException |
                 IllegalBlockSizeException | InvalidKeyException e) {
