ab7d036b6b230d9049efecf76a8af19cd41591c7
Piotr Matras <piotrmatras21@gmail.com>
2020-12-09 20:54:35 +0100

    added async scheduled job for email alerts sending

--- backend/src/main/java/io/licensemanager/backend/BackendApplication.java
+++ backend/src/main/java/io/licensemanager/backend/BackendApplication.java
@@ -2,14 +2,16 @@ package io.licensemanager.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
