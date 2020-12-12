870e3794dfcbfaf7b70f54be61e741e186d2e811
Piotr Matras <piotrmatras21@gmail.com>
2020-12-12 18:03:15 +0100

    added retrieving list of audit logs and available audited operations names

--- backend/src/main/java/io/licensemanager/backend/service/AuditService.java
+++ backend/src/main/java/io/licensemanager/backend/service/AuditService.java
@@ -1,5 +1,6 @@
 package io.licensemanager.backend.service;
 
+import io.licensemanager.backend.configuration.setup.Operation;
 import io.licensemanager.backend.entity.AuditEvent;
 import io.licensemanager.backend.event.SystemOperationEvent;
 import io.licensemanager.backend.repository.AuditEventRepository;
@@ -12,6 +13,9 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -35,5 +39,15 @@ public class AuditService {
 
         auditRepository.save(auditEvent);
     }
-    
+
+    public List<String> getOperationsList() {
+        return Stream.of(Operation.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<AuditEvent> getAuditEventsList() {
+        return auditRepository.findAll();
+    }
 }
